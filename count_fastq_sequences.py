5e682d268c3f9f6774dec6eed8e3f174cd73d151
Stephen Kiniry <s.kiniry@umail.ucc.ie>
2017-10-03 17:32:42 +0100

    add script to count most frequent sequence in a fastq file

---
+++ count_fastq_sequences.py
@@ -0,0 +1,24 @@
+import sys
+import operator
+
+# each key is a sequence, the value for which is the number of times it has been found in the fastq
+count_dict = {}
+
+# first argument is the path to the fastq file
+infile = open(sys.argv[1],"r")
+inlines = infile.readlines()
+infile.close()
+
+# parse every 4th line in the fastq file, starting at line 1
+for seq in inlines[1::4]:
+    if seq.strip("\n") in count_dict:
+        count_dict[seq.strip("\n")] += 1
+    else:
+        count_dict[seq.strip("\n")] = 1
+
+# create a sorted list of key, value pairs (sequences at end of list are most freuquent)
+sorted_count_dict = sorted(count_dict.items(), key=operator.itemgetter(1))
+
+# print the top 10 most frequent sequences and the number of times they were counted
+for tup in sorted_count_dict[-10:]:
+    print "{},{}\n".format(tup[0], tup[1])
